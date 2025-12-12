c10de6e55bc0dbfa0841e21b1267792cdc6fbcf8
suhannoh <suhannoh@gmail.com>
2025-12-12 12:28:13 +0900

    Update CORS origin for LinkController

--- src/main/java/com/example/backend/controller/LinkController.java
+++ src/main/java/com/example/backend/controller/LinkController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://mini-project-0yg2.onrender.com/:5173")
 @RequiredArgsConstructor
 @RequestMapping("/user")
 public class LinkController {
