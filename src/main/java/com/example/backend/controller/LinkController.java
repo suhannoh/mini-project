facccefe287d3a64b13823db0819f25da38ffbc6
suhannoh <suhannoh@gmail.com>
2025-12-12 12:30:10 +0900

    Update CrossOrigin annotation to allow multiple origins

--- src/main/java/com/example/backend/controller/LinkController.java
+++ src/main/java/com/example/backend/controller/LinkController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "https://mini-project-0yg2.onrender.com/:5173")
+@CrossOrigin(origins = {
+    "https://mini-project-0yg2.onrender.com",
+    "http://localhost:5173")
 @RequiredArgsConstructor
 @RequestMapping("/user")
 public class LinkController {
