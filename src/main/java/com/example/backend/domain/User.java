d2c02d9b8433553f43eb5c4c586c018d87348903
suhannoh <suhannoh@gmail.com>
2025-12-13 19:15:47 +0900

    배포 테스트(prod)

--- src/main/java/com/example/backend/domain/User.java
+++ src/main/java/com/example/backend/domain/User.java
@@ -10,12 +10,19 @@ import lombok.NoArgsConstructor;
 @Table(name = "users")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "users_seq")
+    @SequenceGenerator(
+            name = "users_seq",
+            sequenceName = "users_seq",
+            allocationSize = 1
+    )
     private long id;
 
     private String email;
     private String password;
     private String name;
+    @Column(name="phone")
     private String phone;
 
     public User (String email , String password , String name) {
